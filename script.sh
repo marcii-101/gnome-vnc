f01b8090475034a4caf3923e3f84483b3a09f98c
marcii-101 <86745494+marcii-101@users.noreply.github.com>
2021-12-24 15:45:42 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,3 +3,6 @@ sudo apt update && sudo apt install ubuntu-gnome-desktop tigervnc-standalone-ser
 cat << EOF > ~/vnc.sh
 startx /usr/bin/gnome-session --session=gnome -- /usr/bin/Xvnc :1 -SecurityTypes None
 EOF
+chmod +x ~/vnc.sh
+echo "Install complete!"
+echo "Run ~/vnc.sh to start the server!"
