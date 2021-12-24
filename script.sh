850522579d323df4775e536b2b04e84d85edac73
marcii-101 <86745494+marcii-101@users.noreply.github.com>
2021-12-24 15:46:39 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update && sudo apt install ubuntu-gnome-desktop tigervnc-standalone-server tigervnc-viewer -y
 cat << EOF > ~/vnc.sh
-startx /usr/bin/gnome-session --session=gnome -- /usr/bin/Xvnc :1 -SecurityTypes None
+startx /usr/bin/gnome-session --session=gnome -- /usr/bin/Xvnc -SecurityTypes None
 EOF
 chmod +x ~/vnc.sh
 echo "Install complete!"
